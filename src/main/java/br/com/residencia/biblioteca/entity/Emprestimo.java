745a91edde724f693b879b4b5bde890c1ffd1c70
vTekashi <victortiagoferreira@gmail.com>
2022-10-04 20:57:33 -0300

    Primeiro commit

---
+++ src/main/java/br/com/residencia/biblioteca/entity/Emprestimo.java
@@ -0,0 +1,90 @@
+package br.com.residencia.biblioteca.entity;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "emprestimo")
+public class Emprestimo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigoemprestimo")
+	private int codigoEmprestimo;
+	
+	@Column(name = "dataemprestimo")
+	private Instant dataEmprestimo;
+	
+	@Column(name = "dataentrega")
+	private Instant dataEntrega;
+	
+	@Column(name = "valoremprestimo")
+	private BigDecimal valorEmprestimo;
+	
+	@ManyToOne
+	@JoinColumn(name = "numeromatriculaaluno", referencedColumnName = "numeromatriculaaluno")
+	private Aluno alunos;
+	
+	@OneToOne
+	@JoinColumn(name = "codigolivro", referencedColumnName = "codigolivro")
+	private Livro livro; //PERGUNTAR SOBRE O PRIVATE
+
+	public int getCodigoEmprestimo() {
+		return codigoEmprestimo;
+	}
+
+	public void setCodigoEmprestimo(int codigoEmprestimo) {
+		this.codigoEmprestimo = codigoEmprestimo;
+	}
+
+	public Instant getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+
+	public void setDataEmprestimo(Instant dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public Instant getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(Instant dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public BigDecimal getValorEmprestimo() {
+		return valorEmprestimo;
+	}
+
+	public void setValorEmprestimo(BigDecimal valorEmprestimo) {
+		this.valorEmprestimo = valorEmprestimo;
+	}
+
+	public Aluno getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(Aluno alunos) {
+		this.alunos = alunos;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	
+}
