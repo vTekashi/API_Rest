a59dfb9f3efd0c5a28f418a7b3e4995c4ef2d115
vTekashi <victortiagoferreira@gmail.com>
2022-10-05 20:41:08 -0300

    commit de serviços

---
+++ src/main/java/br/com/residencia/biblioteca/service/AlunoService.java
@@ -0,0 +1,54 @@
+package br.com.residencia.biblioteca.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.residencia.biblioteca.entity.Aluno;
+import br.com.residencia.biblioteca.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	@Autowired
+	AlunoRepository alunoRepository;
+	
+	public List<Aluno> getAllAlunos(){
+		return alunoRepository.findAll();
+	}
+	
+	public Aluno getAlunoById(int id) {
+		return alunoRepository.findById(id).get();
+		//return alunoRepository.findById(id).orElse(null);
+	}
+	
+	public Aluno saveAluno(Aluno aluno) {
+		return alunoRepository.save(aluno);
+	}
+	
+	public Aluno updateAluno(Aluno aluno, int id) {
+		/*getAlunoById é o metodo de pegar aluno.*/
+		Aluno alunoExistenteNoBanco = getAlunoById(id);
+		
+		alunoExistenteNoBanco.setBairro(aluno.getBairro());
+		alunoExistenteNoBanco.setCidade(aluno.getCidade());
+		alunoExistenteNoBanco.setComplemento(aluno.getComplemento());
+		alunoExistenteNoBanco.setCpf(aluno.getCpf());
+		alunoExistenteNoBanco.setDataNascimento(aluno.getDataNascimento());
+		alunoExistenteNoBanco.setEmprestimos(aluno.getEmprestimos());
+		alunoExistenteNoBanco.setLogradouro(aluno.getLogradouro());
+		alunoExistenteNoBanco.setNome(aluno.getNome());
+		alunoExistenteNoBanco.setNumeroLogradouro(aluno.getNumeroLogradouro());
+		
+		return alunoRepository.save(alunoExistenteNoBanco);
+	}
+	
+	public Aluno deleteAluno(int id) {
+		alunoRepository.deleteById(id); //DELETA
+		return getAlunoById(id);  //PROCURA NO BANCO PARA VER SE DELETOU.
+	}
+	
+	
+	
+	
+}
